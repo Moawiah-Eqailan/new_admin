6362c2218d502c2cea44dae9f10f1d209fa1f5eb
Moawiah-Eqailan <meqailan5@gmail.com>
2024-11-26 08:23:43 +0300

    fixing bugs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -113,15 +113,15 @@
     <div class="row">
 
         <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+        <!-- <div class="col-xl-8 col-lg-7"> -->
+        <!-- <div class="card shadow mb-4"> -->
+        <!-- Card Header - Dropdown -->
+        <!-- <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
 
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
+                </div> -->
+        <!-- Card Body -->
+        <!-- <div class="card-body">
                     <div class="chart-area">
                         <div class="chartjs-size-monitor">
                             <div class="chartjs-size-monitor-expand">
@@ -135,18 +135,18 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <!-- Pie Chart -->
-        <div class="col-xl-4 col-lg-5">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+        <!-- <div class="col-xl-4 col-lg-5">
+            <div class="card shadow mb-4"> -->
+        <!-- Card Header - Dropdown -->
+        <!-- <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
 
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
+                </div> -->
+        <!-- Card Body -->
+        <!-- <div class="card-body">
                     <div class="chart-pie pt-4 pb-2">
                         <div class="chartjs-size-monitor">
                             <div class="chartjs-size-monitor-expand">
@@ -169,9 +169,9 @@
                             <i class="fas fa-circle text-info"></i> Referral
                         </span>
                     </div>
-                </div>
+                </div> 
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 <script>
